e4507d77c368af7052341d0bfc5a5226e6259e2e
kshuvaev <chamga2010@gmail.com>
2024-12-17 18:39:41 +0500

    Домашнее задание по теме "Генераторные сборки"

---
+++ module_9_3.py
@@ -0,0 +1,10 @@
+#  Домашнее задание по теме "Генераторные сборки"
+
+first = ['Strings', 'Student', 'Computers']
+second = ['Строка', 'Урбан', 'Компьютер']
+
+first_result = (len(i) - len(j) for i, j in zip(first, second) if len(i) != len(j))
+second_result = (len(first[i]) == len(second[i]) for i in range(0, len(first)))
+
+print(list(first_result))
+print(list(second_result))
