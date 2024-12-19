4fab119b59409c7a78e93c6b857b4a05903c0570
kshuvaev <chamga2010@gmail.com>
2024-12-20 00:09:21 +0500

    Домашнее задание по теме "Итераторы"

---
+++ module_9_5.py
@@ -0,0 +1,58 @@
+#Домашнее задание по теме "Итераторы"
+
+class Iterator:
+
+    def __init__(self, start, stop, step=1):
+        self.start = start
+        self.stop = stop
+        self.pointer = start
+        if step == 0:
+            raise StepValueError
+        else:
+            self.step = step
+
+
+    def __iter__(self):
+        self.pointer = self.start
+        return self
+
+    def __next__(self):
+
+        if ((self.pointer <= self.stop and self.step > 0)
+                or (self.pointer >= self.stop and self.step < 0)):
+            self.pointer += self.step
+            return self.pointer-self.step
+        raise StopIteration()
+
+class StepValueError(Exception):
+
+    def __init__(self):
+        return
+
+try:
+    iter1 = Iterator(100, 200, 0)
+    for i in iter1:
+        print(i, end=' ')
+except StepValueError:
+    print('Шаг указан неверно')
+
+iter2 = Iterator(-5, 1)
+iter3 = Iterator(6, 15, 2)
+iter4 = Iterator(5, 1, -1)
+iter5 = Iterator(10, 1)
+
+for i in iter2:
+    print(i, end=' ')
+print()
+
+for i in iter3:
+    print(i, end=' ')
+print()
+
+for i in iter4:
+    print(i, end=' ')
+print()
+
+for i in iter5:
+    print(i, end=' ')
+print()
